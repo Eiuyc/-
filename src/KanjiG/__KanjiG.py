e980b3bed4075bd92db3c901640db1d8e74de76e
Eiuyc <408631569@qq.com>
2021-09-28 15:04:20 +0800

    add

--- src/KanjiG/__KanjiG.py
+++ src/KanjiG/__KanjiG.py
@@ -60,10 +60,11 @@ class Generator():
             image = 255 - image
         return image
 
-    def line(self, image):
-        p1, p2 = map(tuple, np.random.randint(0, self.SIZE, (2,2)))
-        thickness = np.random.randint(1, 5)
-        image = cv2.line(image, p1, p2, self.LINE_COLOR, thickness=thickness)
+    def line(self, image, count_range=[1,3], thickness_range=[1,3]):
+        for _ in range(np.random.randint(*count_range)):
+            p1, p2 = map(tuple, np.random.randint(0, image.shape[:2], (2,2)))
+            thickness = np.random.randint(*thickness_range)
+            image = cv2.line(image, p1, p2, self.LINE_COLOR, thickness=thickness)
         return image
 
     def gen_sample(self):
@@ -71,17 +72,19 @@ class Generator():
         image, label = gen()
         return image, label
 
-    def gen_single(self):
-        index = np.random.randint(len(self.KANJI_LIST))
+    def gen_single(self, blank=False, index=None):
+        if index is None: index = np.random.randint(len(self.KANJI_LIST))
         word = self.KANJI_LIST[index]
+        if blank:
+            index = 0
+            word = ''
         image = Image.fromarray(self.BG)
         draw = ImageDraw.Draw(image)
         draw.text(self.TEXT_LOCATION, word, font=self.get_font(), fill=self.WORD_COLOR)
         image = np.array(image)
         image = self.noise(image)
         image = self.rotate(image)
-        for _ in range(np.random.randint(3)):
-            image = self.line(image)
+        image = self.line(image)
         label = {
             'word': word,
             'index': index
@@ -93,9 +96,11 @@ class Generator():
         words = []
         for i in range(self.GRID[0]):
             for j in range(self.GRID[1]):
-                img, label = self.gen_single()
+                blank = np.random.randint(2)
+                img, label = self.gen_single(blank)
                 location = i*self.SIZE, j*self.SIZE
                 image[location[0]:location[0]+self.SIZE, location[1]:location[1]+self.SIZE] = img
+                if blank: continue
                 word = label['word']
                 index = label['index']
                 size = self.SIZE, self.SIZE
@@ -109,8 +114,26 @@ class Generator():
             'words': words
         }
         image = self.noise(image, 250)
+        image = self.line(image)
         return image, label
 
+    def label2cxywh(self, size, label):
+        words = label['words']
+        def word2line(word):
+            c = word['index']
+            print(word['word'], c)
+            h, w = word['size']
+            y, x = word['location']
+            x += w/2
+            y += h/2
+            y /= size[0]
+            h /= size[0]
+            x /= size[1]
+            w /= size[1]
+            s = f'{c} {x:.4f} {y:.4f} {w:.4f} {h:.4f}\n'
+            return s
+        l = list(map(word2line, words))
+        return l
 
 if __name__ == '__main__':
     g = Generator()
